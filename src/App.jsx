8de77fe8a84b2282a34c7cbe7f714957471802ed
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-20 15:06:16 +0100

    Add input and button for movie search

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react"
+
 function App() {
+
+  const initial_Api_Url = "https://api.themoviedb.org/3/search/movie"
+
+  const [search, setSearch] = useState("")
+
+  function handleSearch() {
+    console.log(search);
+
+  }
 
   return (
     <>
       <h1>Boolflix</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
     </>
   )
 }
 
 export default App
+
+
+{/*
+  
+  1. creo input
+     - il valore corrisponde a ciò che scrivo
+  2. creo bottone
+     - al click del bottone viene richiamata la funzione handleSearch che mi fa vedere il risultato in console
+
+*/}
